db864db80e394dc33e3a80c8ddfac3bdc1bd14cd
Vitor <vitor@itangast.(none)>
2013-09-18 13:50:49 +0200

    Add management command to import old db.

---
+++ loco_app/management/commands/import_old_db.py
@@ -0,0 +1,52 @@
+import MySQLdb
+
+from django.core.management.base import BaseCommand, CommandError
+
+from loco_app.models import *
+
+#from polls.models import Poll
+
+class Command(BaseCommand):
+    def connect(self):
+        db = MySQLdb.connect(host="my2.ortoloco.ch",
+                             user="my2ortoloco",
+                             passwd="My20rt0l0c0",
+                             db="my2ortoloco")
+
+        self.cur = db.cursor()
+
+    def query(self, querystring):
+        self.cur.execute(querystring)
+        for row in self.cur.fetchall():
+            yield row
+
+
+    # entry point used by manage.py
+    def handle(self, *args, **options):
+        self.connect()
+
+        new_users = []
+        for row in self.query("SELECT * FROM usr"):
+            email=row[0]
+
+            # convert from latin-1 encoded string to unicode object, which is 
+            # automagically handled by Django
+            email = email.decode("latin-1")
+
+            user = User(username=email)
+            new_users.append(user)
+
+
+        # get existing users from db
+        old_users = set(User.objects.all())
+
+        # bulk_create groups everything into a single query. However, Post-create events won't be sent
+        # so Loco instances have to be created manually.
+        User.objects.bulk_create(new_users)
+
+        # get fresh user objects back from db, now with primary keys etc.
+        new_users = set(User.objects.all()) - old_users
+
+        # create loco for each new user
+        Loco.objects.bulk_create(Loco(user=user) for user in new_users)
+
