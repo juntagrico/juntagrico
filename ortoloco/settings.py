7935bf319694aa35503ee589d95b4a2b476bff20
Oliver Ganz <oliver.ganz@gmail.com>
2013-10-03 09:48:17 +0200

    Fixed a static path for xiala hosting

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -52,7 +52,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '/static/medias/')
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static/medias/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
