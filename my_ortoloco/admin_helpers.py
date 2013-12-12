9a5de544fe9cf3bbe86f8a77b726e5db4b6d0500
Vitor <vitor@itangast.(none)>
2013-12-13 00:20:01 +0100

    Fix bug in MyHTMLWidget.

--- my_ortoloco/admin_helpers.py
+++ my_ortoloco/admin_helpers.py
@@ -10,6 +10,11 @@ class MyHTMLWidget(forms.widgets.Widget):
     Widget that display (non-editably) arbitrary html.
     """
     def render(self, name, value, attrs=None):
+        if value is None:
+            # This is needed because admin sometimes doesn't supply a value,
+            # e.g. when a bad input in some other field causes the form to re-render with error
+            # messages
+            return ""
         return format_html(value)
 
 
