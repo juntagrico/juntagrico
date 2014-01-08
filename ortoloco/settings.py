cf30e3104e2aa36aaa2b66ab86b299e1718c982b
Oliver Ganz <oliver.ganz@gmail.com>
2014-01-08 22:02:34 +0100

    addded to hosts

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -34,7 +34,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.orto.xiala.net']
+ALLOWED_HOSTS = ['.orto.xiala.net', '.ortoloco.ch']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
