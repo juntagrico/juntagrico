aeccb634e32b056106d8d82a7a29b363a7d43a44
Oliver Ganz <oliver.ganz@gmail.com>
2014-01-25 09:18:28 +0100

    settings wie sie auf dem server sind

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -182,6 +182,30 @@ INSTALLED_APPS = (
     'impersonate'
 )
 
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'ortoloco.ch_python.log': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(os.path.dirname(BASE_DIR), 'logs/ortoloco.ch_python.log'),#'/Users/og/Stuff/ortoloco/orltoloco.log',
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['ortoloco.ch_python.log'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
 GALLERY_SAMPLE_SIZE = 4
 
 from settings_local import *
