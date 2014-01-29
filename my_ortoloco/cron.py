63c89546ad887e740be1df073a3b209c4faa468d
Ortwin Glück <rogu@logobject.ch>
2014-01-29 14:41:05 +0100

    optimize data access
    Don't load all Jobs ever created every 10 minutes. That doesn't scale.
    Let the DB do the filtering. It's very good at that :-)

--- my_ortoloco/cron.py
+++ my_ortoloco/cron.py
@@ -9,19 +9,18 @@ class Send_Job_Reminders(Job):
     run_every = 60 # every minute, but the cron library does it every 10minutes so it gets executed between 10 and 11 minutes
 
     def job(self):
-        for job in models.Job.objects.all():
-            needs_reminder = job.time - datetime.timedelta(1) < datetime.datetime.now()
-            is_past = job.time < datetime.datetime.now()
-            if not job.reminder_sent and needs_reminder and not is_past:
-                participants = []
-                emails = []
-                for bohne in models.Boehnli.objects.filter(job_id=job.id):
-                    if bohne.loco is not None:
-                        participants.append(str(bohne.loco))
-                        emails.append(bohne.loco.email)
-                send_job_reminder(emails, job, ", ".join(participants), "http://my.ortoloco.ch")
-                job.reminder_sent = True
-                job.save()
+        now = datetime.datetime.now();
+        end = now + datetime.timedelta(1);
+        for job in models.Job.objects.filter(time__range=(now, end), reminder_sent__exact==False):
+            participants = []
+            emails = []
+            for bohne in models.Boehnli.objects.filter(job_id=job.id):
+                if bohne.loco is not None:
+                    participants.append(str(bohne.loco))
+                    emails.append(bohne.loco.email)
+            send_job_reminder(emails, job, ", ".join(participants), "http://my.ortoloco.ch")
+            job.reminder_sent = True
+            job.save()
 
 
 
