4a6da6c49e2fd42b55563634619a32e346678518
rbf <rob@sixsq.com>
2014-09-13 13:42:53 +0200

    new: update 'send email' button with real count in filter pages, #22

--- static/js/initMyAbos.js
+++ static/js/initMyAbos.js
@@ -2,10 +2,32 @@
 define([], function () {
     var dable = new Dable("filter-table");
 
+    var OriginalUpdateDisplayedRows = dable.UpdateDisplayedRows;
+    dable.UpdateDisplayedRows = function (body) {
+        OriginalUpdateDisplayedRows(body);
+        updateSendEmailButton(dable.VisibleRowCount());
+    };
+
+    function updateSendEmailButton(count) {
+        if (count == 0) {
+            $("button#send-email")
+                .prop('disabled', true)
+                .text("Email senden");
+        } else if (count == 1) {
+            $("button#send-email")
+                .prop('disabled', false)
+                .text("Email an die Inhaber dieses Abo senden");
+        } else {
+            $("button#send-email")
+                .prop('disabled', false)
+                .text("Email an die Inhaber dieser " + dable.VisibleRowCount() + " Abos senden");
+        }
+    }
+
     dable.UpdateDisplayedRows();        // Update the table
     dable.UpdateStyle();                // Reapply our styles
 
-    $("#emailSender").submit(function( event ) {
+    $("form#email-sender").submit(function( event ) {
         var emails = [];
         $("#filter-table").find("tr").each(function () {
             var txt = $("td:eq(5)", this).text().trim();
--- static/js/initMyFilters.js
+++ static/js/initMyFilters.js
@@ -2,10 +2,32 @@
 define([], function () {
     var dable = new Dable("filter-table");
 
+    var OriginalUpdateDisplayedRows = dable.UpdateDisplayedRows;
+    dable.UpdateDisplayedRows = function (body) {
+        OriginalUpdateDisplayedRows(body);
+        updateSendEmailButton(dable.VisibleRowCount());
+    };
+
+    function updateSendEmailButton(count) {
+        if (count == 0) {
+            $("button#send-email")
+                .prop('disabled', true)
+                .text("Email senden");
+        } else if (count == 1) {
+            $("button#send-email")
+                .prop('disabled', false)
+                .text("Email an diesem Loco senden");
+        } else {
+            $("button#send-email")
+                .prop('disabled', false)
+                .text("Email an diese " + dable.VisibleRowCount() + " Locos senden");
+        }
+    }
+
     dable.UpdateDisplayedRows();        // Update the table
     dable.UpdateStyle();                // Reapply our styles
 
-    $("#emailSender").submit(function( event ) {
+    $("form#email-sender").submit(function( event ) {
         var emails = [];
         $("#filter-table").find("tr").each(function () {
             var txt = $("td:eq(4)", this).text().trim();
