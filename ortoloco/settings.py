d1f9168dbc0b3277514bc9ac7c56658951c48a47
oliverganz <oliver.ganz@gmail.com>
2015-08-16 17:47:54 +0200

    simplify => static files serving must be checked

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -85,7 +85,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = os.environ.get('ORTOLOCO_STATIC_PATH', '/static/tosomeNotWorkingUrl/')
+STATIC_URL = '/static/tosomeNotWorkingUrl/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
