624239eb5142bd072fd40c5bcee0952e1e8248f8
oliverganz <oliver.ganz@gmail.com>
2015-08-16 14:49:54 +0200

    no settings.local for db anymore => use the environment variables instead

--- settings_local.py
+++ settings_local.py
@@ -1,29 +1,6 @@
 import os
 from ortoloco.settings import *
 
-DATABASES = {
-    'default': {
-
-        # To use with MySql
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'mysql_db_name',
-
-        # To use with sqlite3
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite', # Or path to database file if using sqlite3.
-
-        # To use with oracle
-        # 'ENGINE': 'django.db.backends.oracle',
-        # 'NAME': 'oracle_db_name',
-
-        # The following settings are not used with sqlite3:
-        # 'USER': 'django',
-        # 'PASSWORD': 'django',
-        #'HOST': 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        #'PORT': '',          # Set to empty string for default.
-    }
-}
-
 STATIC_URL = '/static/tosomeotherurl/'
 
 LOGGING = {
