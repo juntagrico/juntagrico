0b853fc2d7d562b4402b4c55e5616df8a981b21d
oliverganz <oliver.ganz@gmail.com>
2015-09-30 22:46:35 +0200

    Revert "change: maintenance modus on"
    This reverts commit 911b39fbeddeaed59762105905ec1007b45d461b.

--- ortoloco/urls.py
+++ ortoloco/urls.py
@@ -88,7 +88,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     #(r'^tinymce/', include('tinymce.urls')),
     url(r'^medias/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT,
