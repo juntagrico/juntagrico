b9ed9c179b93b3f2eb615b785959e9c06b41fcbb
Thomas Fahrni <thomasfahrni@gmx.ch>
2015-10-08 22:27:36 +0200

    enabled timezone handling

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -63,7 +63,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = False
+USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
