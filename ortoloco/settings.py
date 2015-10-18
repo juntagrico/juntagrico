a05cba81a68fc2f534c46f6efdb8803176cfe90e
oliverganz <oliver.ganz@gmail.com>
2015-10-18 20:57:53 +0200

    add aws s3 storage info

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -231,3 +231,29 @@ LOGGING = {
 }
 
 GALLERY_SAMPLE_SIZE = 4
+
+#os.environ['S3_USE_SIGV4'] = 'True'
+
+# S3Boto storage settings for photologue example project.
+
+DEFAULT_FILE_STORAGE = 'ortoloco.utils.MediaS3BotoStorage'
+#STATICFILES_STORAGE = 'ortoloco.utils.StaticS3BotoStorage'
+
+try:
+    AWS_ACCESS_KEY_ID = os.environ['ORTOLOCO_AWS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['ORTOLOCO_AWS_KEY']
+    AWS_STORAGE_BUCKET_NAME = os.environ['ORTOLOCO_AWS_BUCKET_NAME']
+except KeyError:
+    raise KeyError('Need to define AWS environment variables: ' +
+                   'AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_STORAGE_BUCKET_NAME')
+
+# Default Django Storage API behavior - don't overwrite files with same name
+AWS_S3_FILE_OVERWRITE = False
+
+MEDIA_ROOT = 'media/'
+MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+
+#STATIC_ROOT = '/static/'
+#STATIC_URL = 'http://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
+
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
