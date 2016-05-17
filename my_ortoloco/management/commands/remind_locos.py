56ba62f6dd8492d98ec1634c9831e52a1f66c506
David Simmen <david.simmen@bluewin.ch>
2016-05-17 21:29:42 +0200

    #53 added imports to command
     all necessary imports for the command

--- my_ortoloco/management/commands/remind_locos.py
+++ my_ortoloco/management/commands/remind_locos.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 
 from my_ortoloco.models import *
-from my_ortoloco.model_audit import *
-
-from django.contrib.auth.models import *
+from django.utils import timezone
+import datetime
+from my_ortoloco.mailer import send_job_reminder
 
 
 class Command(BaseCommand):
