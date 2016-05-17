e2fda9c2c4843ebf084ef7a53185ea0cb7fc427d
David Simmen <david.simmen@bluewin.ch>
2016-05-17 21:57:58 +0200

    #53 added command body
     filled command with code

--- my_ortoloco/management/commands/remind_locos.py
+++ my_ortoloco/management/commands/remind_locos.py
@@ -11,6 +11,18 @@ class Command(BaseCommand):
     # entry point used by manage.py
     def handle(self, *args, **options):
 
-        
+        now = datetime.datetime.now()
+        end = now + datetime.timedelta(days=1)
+        for job in Job.objects.filter(time__range=(now, end), reminder_sent__exact=False):
+            participants = []
+            emails = []
+            for bohne in Boehnli.objects.filter(job_id=job.id):
+                if bohne.loco is not None:
+                    participants.append(str(bohne.loco))
+                    emails.append(bohne.loco.email)
+            send_job_reminder(emails, job, ", ".join(participants), "http://my.ortoloco.ch")
+            job.reminder_sent = True
+            job.save()
+            print("reminder sent for job " + str(job.id))
 
 
