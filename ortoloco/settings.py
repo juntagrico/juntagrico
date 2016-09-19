60c2205fd94497eae5010a5e5580fafe90e4893e
David Simmen <david.simmen@bluewin.ch>
2016-09-19 22:17:02 +0200

    fix static (again)

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -110,7 +110,7 @@ STATICFILES_DIRS = (
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
-    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
