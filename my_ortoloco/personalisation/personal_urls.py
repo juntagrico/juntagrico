f934104625cf7ee9a91e2c7d92f94ab48906f97d
David Simmen <david.simmen@bluewin.ch>
2016-09-19 22:03:34 +0200

    upgrade to newer versions maintenance

--- my_ortoloco/personalisation/personal_urls.py
+++ my_ortoloco/personalisation/personal_urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from my_ortoloco.personalisation import personal_views as my_ortoloco_personal
 
 urlpatterns = [
