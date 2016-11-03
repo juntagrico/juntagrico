d7b571c3b969a2b08e5ee5fb955d0d90267bdd6d
motu55 <david.simmen@bluewin.ch>
2016-11-03 21:41:32 +0100

    #117 migration dependency was wrong

--- my_ortoloco/migrations/0017_auto_20161102_2125.py
+++ my_ortoloco/migrations/0017_auto_20161102_2125.py
@@ -25,7 +25,7 @@ def data_migration(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('my_ortoloco', '0016_auto_20161102_2123'),
+        ('my_ortoloco', '0016_auto_20161103_2038'),
     ]
 
     operations = [
