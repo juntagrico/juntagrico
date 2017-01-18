9fc02f7f5f77453d74e2b1b157289d54538c833c
motu55 <david.simmen@bluewin.ch>
2017-01-18 09:38:37 +0100

    seqarch box for politoloco admin

--- static_ortoloco/admin.py
+++ static_ortoloco/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from static_ortoloco.models import *
 
+class PolitolocoAdmin(admin.ModelAdmin):
+    search_fields = ('email',)
+
 admin.site.register(StaticContent)
 admin.site.register(Media)
 admin.site.register(Download)
 admin.site.register(Link)
 admin.site.register(Document)
-admin.site.register(Politoloco)
+admin.site.register(Politoloco, PolitolocoAdmin)
