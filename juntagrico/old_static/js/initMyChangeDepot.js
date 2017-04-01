7002e1cf218750c27abd895be40a678ad90ec80f
David Simmen <david.simmen@bluewin.ch>
2017-04-01 12:04:48 +0200

    celanup before the big fork

--- juntagrico/old_static/js/initMyChangeDepot.js
+++
@@ -1,9 +0,0 @@
-/*global define, $, loco_scheine, depots, destinations, google */
-define(['modules/depotDistance'], function (depotDistance) {
-
-    // preselect depot
-    $("#depot").val(depot_id);
-
-    depotDistance.calculate(loco_addr, destinations, depots);
-
-});
