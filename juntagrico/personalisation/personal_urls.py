5eab6d20e69bb0645662054cab03f985ddbea974
David Simmen <david.simmen@bluewin.ch>
2017-04-01 17:27:28 +0200

    removed old personalisation stuff which is not used anymore

--- juntagrico/personalisation/personal_urls.py
+++ juntagrico/personalisation/personal_urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import include, url
 from juntagrico.personalisation import personal_views as juntagrico_personal
 
 urlpatterns = [
-    url('^politoloco$', juntagrico_personal.send_politoloco),
 ]
