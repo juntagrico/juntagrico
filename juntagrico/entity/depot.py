b315d7c9eaf1580c7b90f4602620248c3014cf09
David Simmen <david.simmen@bluewin.ch>
2017-04-21 13:56:32 +0200

    more code readability

---
+++ juntagrico/entity/depot.py
@@ -0,0 +1,75 @@
+# encoding: utf-8
+
+from django.core import validators
+from django.core.exceptions import ValidationError
+from django.db import models
+from juntagrico.util.temporal import *
+
+from juntagrico.dao.subscriptionsizedao import SubscriptionSizeDao
+from juntagrico.dao.extrasubscriptiontypedao import ExtraSubscriptionTypeDao
+from juntagrico.dao.extrasubscriptioncategorydao import ExtraSubscriptionCategoryDao
+
+class Depot(models.Model):
+    """
+    Location where stuff is picked up.
+    """
+    code = models.CharField("Code", max_length=100, validators=[validators.validate_slug], unique=True)
+    name = models.CharField("Depot Name", max_length=100, unique=True)
+    contact = models.ForeignKey("Member", on_delete=models.PROTECT)
+    weekday = models.PositiveIntegerField("Wochentag", choices=weekday_choices)
+    latitude = models.CharField("Latitude", max_length=100, default="")
+    longitude = models.CharField("Longitude", max_length=100, default="")
+
+    addr_street = models.CharField("Strasse", max_length=100)
+    addr_zipcode = models.CharField("PLZ", max_length=10)
+    addr_location = models.CharField("Ort", max_length=50)
+
+    description = models.TextField("Beschreibung", max_length=1000, default="")
+
+    overview_cache = None
+    subscription_cache = None
+
+    def __unicode__(self):
+        return u"%s %s" % (self.id, self.name)
+
+    def active_subscriptions(self):
+        return self.subscription_set.filter(active=True)
+
+    @property
+    def weekday_name(self):
+        day = "Unbekannt"
+        if 8 > self.weekday > 0:
+            day = weekdays[self.weekday]
+        return day
+
+    @staticmethod
+    def subscription_amounts(subscriptions, name):
+        amount = 0
+        for subscription in subscriptions.all():
+            amount += subscription.subscription_amount(name)
+        return amount
+
+    @staticmethod
+    def extra_subscription(subscriptions, code):
+        amount = 0
+        for subscription in subscriptions.all():
+            amount += len(subscription.extra_subscriptions.all().filter(type__name=code))
+        return amount
+
+    def fill_overview_cache(self):
+        self.fill_active_subscription_cache()
+        self.overview_cache = []
+        for subscription_size in SubscriptionSizeDao.sizes_for_depot_list():
+            self.overview_cache.append(self.subscription_amounts(self.subscription_cache, subscription_size.name))
+        for category in ExtraSubscriptionCategoryDao.all_categories_ordered():
+            for extra_subscription in ExtraSubscriptionTypeDao.extra_types_by_category_ordered(category):
+                code = extra_subscription.name
+                self.overview_cache.append(self.extra_subscription(self.subscription_cache, code))
+
+    def fill_active_subscription_cache(self):
+        self.subscription_cache = self.active_subscriptions()
+
+    class Meta:
+        verbose_name = "Depot"
+        verbose_name_plural = "Depots"
+        permissions = (('is_depot_admin', 'Benutzer ist Depot Admin'),)
---
+++ juntagrico/entity/jobs.py
@@ -0,0 +1,285 @@
+# encoding: utf-8
+
+from django.db import models
+from polymorphic.models import PolymorphicModel
+
+from juntagrico.dao.assignmentdao import AssignmentDao
+
+from juntagrico.util.jobs import *
+from juntagrico.config import Config
+
+class ActivityArea(models.Model):
+    name = models.CharField("Name", max_length=100, unique=True)
+    description = models.TextField("Beschreibung", max_length=1000, default="")
+    core = models.BooleanField("Kernbereich", default=False)
+    hidden = models.BooleanField("versteckt", default=False)
+    coordinator = models.ForeignKey("Member", on_delete=models.PROTECT)
+    members = models.ManyToManyField("Member", related_name="areas", blank=True)
+    show_coordinator_phonenumber = models.BooleanField("Koordinator Tel Nr Veröffentlichen", default=False)
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
+    def contact(self):
+        if self.show_coordinator_phonenumber is True:
+            return self.coordinator.phone + "   " + self.coordinator.mobile_phone
+        else:
+            return self.coordinator.email
+
+    class Meta:
+        verbose_name = 'Tätigkeitsbereich'
+        verbose_name_plural = 'Tätigkeitsbereiche'
+        permissions = (('is_area_admin', 'Benutzer ist TätigkeitsbereichskoordinatorIn'),)
+
+class JobExtraType(models.Model):
+    """
+    Types of extras which a job type might need or can have
+    """    
+    name = models.CharField("Name", max_length=100, unique=True)
+    display_empty = models.CharField("Icon für felhlendes Extra", max_length=1000, blank=False, null=False)
+    display_full = models.CharField("Icon für Extra", max_length=1000, blank=False, null=False)
+    
+    class Meta:
+        verbose_name = 'JobExtraTyp'
+        verbose_name_plural = 'JobExtraTypen'
+
+class JobExtra(models.Model):
+    """
+    Actual Extras mapping
+    """
+    recuring_type = models.ForeignKey("JobType", related_name="job_extras_set", null=True,
+                                          blank=True,
+                                          on_delete=models.PROTECT)
+    onetime_type = models.ForeignKey("OneTimeJob", related_name="job_extras_set", null=True,
+                                          blank=True,
+                                          on_delete=models.PROTECT)
+    extra_type = models.ForeignKey("JobExtraType", related_name="job_types_set", null=False,
+                                          blank=False,
+                                          on_delete=models.PROTECT)
+    per_member = models.BooleanField("jeder kann Extra auswählen", default=False)
+
+    def empty(self,assignment_set):
+        ids=[assignment.id for assignment in assignment_set]
+        return self.assignments.filter(id__in=ids).count()==0
+
+    @property
+    def type(self):
+        if recuring_type is not None:
+            return recuring_type
+        return onetime_type
+    
+    class Meta:
+        verbose_name = 'JobExtra'
+        verbose_name_plural = 'JobExtras'
+
+
+class AbstractJobType(models.Model):
+    """
+    Abstract type of job.
+    """
+    name = models.CharField("Name", max_length=100, unique=True)
+    displayed_name = models.CharField("Angezeigter Name", max_length=100, blank=True, null=True)
+    description = models.TextField("Beschreibung", max_length=1000, default="")
+    activityarea = models.ForeignKey(ActivityArea, on_delete=models.PROTECT)
+    duration = models.PositiveIntegerField("Dauer in Stunden")
+    location = models.CharField("Ort", max_length=100, default="")
+
+    def __unicode__(self):
+        return u'%s - %s' % (self.activityarea, self.get_name())
+
+    def get_name(self):
+        if self.displayed_name is not None and self.displayed_name != '':
+            return self.displayed_name
+        return self.name
+
+    class Meta:
+        verbose_name = 'AbstractJobart'
+        verbose_name_plural = 'AbstractJobarten'
+        abstract = True
+
+
+class JobType(AbstractJobType):
+    """
+    Recuring type of job. do not add field here do it in the parent
+    """
+
+    class Meta:
+        verbose_name = 'Jobart'
+        verbose_name_plural = 'Jobarten'
+
+
+class Job(PolymorphicModel):
+    slots = models.PositiveIntegerField("Plaetze")
+    time = models.DateTimeField()
+    pinned = models.BooleanField(default=False)
+    reminder_sent = models.BooleanField("Reminder verschickt", default=False)
+    canceled = models.BooleanField("abgesagt", default=False)
+    old_canceled = None
+    old_time = None
+
+    @property
+    def type(self):
+        raise NotImplementedError
+
+    def __unicode__(self):
+        return u'Job #%s' % self.id
+
+    def weekday_name(self):
+        weekday = weekdays[self.time.isoweekday()]
+        return weekday[:2]
+
+    def time_stamp(self):
+        return int(time.mktime(self.time.timetuple()) * 1000)
+
+    def freie_plaetze(self):
+        if not (self.slots is None):
+            return self.slots - self.occupied_places()
+        else:
+            return 0
+
+    def end_time(self):
+        return self.time + timezone.timedelta(hours=self.type.duration)
+
+    def start_time(self):
+        return self.time
+
+    def occupied_places(self):
+        return self.assignment_set.count()
+
+    def get_status_percentage(self):
+        assignments = AssignmentDao.assignments_for_job(self.id)
+        if self.slots < 1:
+            return get_status_image(100)
+        return get_status_image(assignments.count() * 100 / self.slots)
+
+    def is_core(self):
+        return self.type.activityarea.core
+
+    def extras(self):
+        extras_result =[]
+        for extra in self.type.job_extras_set.all():
+            if extra.empty(self.assignment_set.all()):
+                extras_result.append(extra.extra_type.display_empty)
+            else:           
+                extras_result.append(extra.extra_type.display_full)
+        return " ".join(extras_result)
+
+    def empty_per_job_extras(self):
+        extras_result =[]
+        for extra in self.type.job_extras_set.filter(per_member=False):
+            if extra.empty(self.assignment_set.all()):
+                extras_result.append(extra)
+        return extras_result
+
+    def full_per_job_extras(self):
+        extras_result =[]
+        for extra in self.type.job_extras_set.filter(per_member=False):
+            if not extra.empty(self.assignment_set.all()):
+                extras_result.append(extra)
+        return extras_result
+
+    def per_member_extras(self):
+        return self.type.job_extras_set.filter(per_member=True)
+
+    def clean(self):
+        if self.old_canceled != self.canceled and self.old_canceled is True:
+            raise ValidationError(u'Abgesagte jobs koennen nicht wieder aktiviert werden', code='invalid')
+   
+
+    @classmethod
+    def pre_save(cls, sender, instance, **kwds):
+        if instance.old_canceled != instance.canceled and instance.old_canceled is False:
+            assignments = AssignmentDao.assignments_for_job(instance.id)
+            emails = set()
+            for assignment in assignments:
+                emails.add(assignment.member.email)
+            instance.slots = 0
+            if len(emails) > 0:
+                send_job_canceled(emails, instance, Config.server_url())
+        if instance.old_time != instance.time:
+            assignments = AssignmentDao.assignments_for_job(instance.id)
+            emails = set()
+            for assignment in assignments:
+                emails.add(assignment.member.email)
+            if len(emails) > 0:
+                send_job_time_changed(emails, instance, Config.server_url())
+
+    @classmethod
+    def post_init(cls, sender, instance, **kwds):
+        instance.old_time = instance.time
+        instance.old_canceled = instance.canceled
+        if instance.canceled:
+            assignments = AssignmentDao.assignments_for_job(instance.id)
+            assignments.delete()
+
+    class Meta:
+        verbose_name = 'AbstractJob'
+        verbose_name_plural = 'AbstractJobs'
+
+
+class RecuringJob(Job):
+    type = models.ForeignKey(JobType, on_delete=models.PROTECT)
+
+    @classmethod
+    def pre_save(cls, sender, instance, **kwds):
+        Job.pre_save(sender, instance)
+
+    @classmethod
+    def post_init(cls, sender, instance, **kwds):
+        Job.post_init(sender, instance)
+
+    class Meta:
+        verbose_name = 'Job'
+        verbose_name_plural = 'Jobs'
+
+
+class OneTimeJob(Job, AbstractJobType):
+    """
+    One time job. Do not add Field here do it in the Parent class
+    """
+
+    @property
+    def type(self):
+        return self
+
+    def __unicode__(self):
+        return u'%s - %s' % (self.activityarea, self.get_name())
+
+    @classmethod
+    def pre_save(cls, sender, instance, **kwds):
+        Job.pre_save(sender, instance)
+
+    @classmethod
+    def post_init(cls, sender, instance, **kwds):
+        Job.post_init(sender, instance)
+
+    class Meta:
+        verbose_name = 'EinzelJob'
+        verbose_name_plural = 'EinzelJobs'
+
+
+class Assignment(models.Model):
+    """
+    Single assignment (work unit).
+    """
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    member = models.ForeignKey("Member", on_delete=models.PROTECT)
+    core_cache = models.BooleanField("Kernbereich", default=False) 
+    job_extras = models.ManyToManyField(JobExtra,related_name='assignments', blank=True)
+
+    def __unicode__(self):
+        return u'%s #%s' % (Config.assignment_string(), self.id)
+
+    def time(self):
+        return self.job.time
+
+    def is_core(self):
+        return self.job.type.activityarea.core
+
+    @classmethod
+    def pre_save(cls, sender, instance, **kwds):
+        instance.core_cache = instance.is_core()
+
+    class Meta:
+        verbose_name = Config.assignment_string()
+        verbose_name_plural = Config.assignments_string()
