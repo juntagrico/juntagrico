e88eb187f88e577d0e9e32c6d98483282a9d9be9
David Simmen <david.simmen@bluewin.ch>
2017-05-08 10:57:49 +0200

    missing pieces

---
+++ juntagrico/management/commands/send_bills.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone 
+from django.utils import bills 
+
+from juntagrico.config import Config
+
+from juntagrico.models import *
+from juntagrico.dao.extrasubbillingperioddao import ExtraSubBillingPeriodDao
+from juntagrico.dao.subscriptiondao import SubscriptionDao
+
+
+class Command(BaseCommand):
+
+    # entry point used by manage.py
+    def handle(self, *args, **options):
+        now = timezone.now()
+        periods = ExtraSubBillingPeriodDao.get_starting_for_date(now)
+        for period in periods:
+            for extra in period.type.extra_subscriptions.filter(active=True):
+                bill_extra_subscription(extra, period)
+        bs = Config.business_year_start()
+        if now.day==bs['day'] and now.month==bs['month']:
+            for subscription in SubscriptionDao.all_active_subscritions():
+                bill_subscription(subscrtption)
