ed5a6168a930d2b18c6627f662bad5589af04a78
David Simmen <david.simmen@bluewin.ch>
2017-10-07 15:09:55 +0200

    fixed the start page issue with the jobs overview

--- juntagrico/static/js/initHomeJobs.js
+++ juntagrico/static/js/initHomeJobs.js
@@ -1,37 +1,13 @@
 /*global define*/
 define([], function () {
-
-    $("#filter-table thead th").each(function () {
-        var title = $(this).text();
-        $(this).append("<input type='text' placeholder='' style='width: 100%;' class='form-control input-sm' />");
-    });
-
+    
     var table = $("#filter-table").DataTable({
         "paging": false,
         "info": false,
         "ordering": false,
-        "search": {
-            "regex": true,
-            "smart": false
-        },
-        "language": {
-            "search": "Suchen: "
-        }
+        "searching": false,
     });
 
-    table.columns().every(function () {
-        var that = this;
-        $("input", this.header()).on("keyup change", function () {
-            if (that.search() !== this.value) {
-                that.search(this.value).draw();
-            }
-        });
-        $("input", this.header()).on("click", function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-        });
-    });
-    
     function format(place,starttime,endtime,area) {
       return '<div>Zeit: ' + starttime + '-' + endtime + '<br/>Ort: ' +place + '<br/>Bereich: ' +area + '</div>';
     }
