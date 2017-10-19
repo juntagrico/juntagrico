96cd42bc40e7a5bd8d08fc66b8b64b0e0cf1b750
David Simmen <david.simmen@bluewin.ch>
2017-10-19 19:43:45 +0200

    fucking shitty fucking javascript errors

--- juntagrico/static/js/slider.js
+++ juntagrico/static/js/slider.js
@@ -7,11 +7,14 @@ $(function () {
         var id = $(this).attr('id');
         if(!id)
             id="myonoffswitch"+name;
+        var valstring = ''
+        if(val!==undefined)
+            valstring= 'value="'+val+'"'
         var checked_string =''
         if(checked)
             checked_string='checked="checked"';
         $(this).replaceWith('<div class="onoffswitch">'+
-                            '<input type="checkbox" name="'+name+'" value="'+val+'" '+ checked_string +' class="onoffswitch-checkbox" id="'+id+'">'+
+                            '<input type="checkbox" name="'+name+'" '+valstring+' '+ checked_string +' class="onoffswitch-checkbox" id="'+id+'">'+
                             '<label class="bg-success onoffswitch-label" for="'+id+'">'+
                             '<span class="onoffswitch-inner"></span>'+
                             '<span class="onoffswitch-switch"></span></label></div>');
