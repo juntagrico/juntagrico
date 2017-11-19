4b3381741de11276eb0e6c9c9f9a758915e6b7f8
David Simmen <david.simmen@bluewin.ch>
2017-11-19 20:55:19 +0100

    admin menu search added

--- juntagrico/apps.py
+++ juntagrico/apps.py
@@ -1,3 +1,5 @@
+import importlib
+
 from django.apps import AppConfig
 from django.conf import settings
 
@@ -5,8 +7,18 @@ class JuntagricoAppConfig(AppConfig):
     name = 'juntagrico'
 
     def ready(self):
+        apps = []
+        
+        admin_menu_templates = []
+        
         print('juntagrico searchching for addons:')
         
         for app in settings.INSTALLED_APPS:
             if app.startswith('juntagrico') and app !='juntagrico':
                 print('found: ' + app) 
+                apps.append(app)
+        
+        print('searching for admin menus')
+        for app in apps:
+            if importlib.util.find_spec('admin_menu_template', app + '.juntagricoapp') is not None:
+                admin_menu_template.append(app + '.juntagricoapp' + 'admin_menu_template'())
