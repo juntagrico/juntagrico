a149b4a798b592194ef1fd3f81f517b0bce6a856
David Simmen <david.simmen@bluewin.ch>
2017-11-22 09:04:43 +0100

    xls stuff for more general handling

--- juntagrico/util/xls.py
+++ juntagrico/util/xls.py
@@ -7,8 +7,34 @@ from django.http import HttpResponse
     Generates excell for a defined set of fields and a model
 '''
 
+def generate_excell_from_model(fields, model_instance):
+    parsed_fields={}
+    for field in fields:
+        parts = field.split('.')
+        count = 1
+        dbfield = model_instance._meta.get_field(parts[0])
+        while count < len(parts):
+            dbfield = dbfield.related_model._meta.get_field(parts[count])
+            count += 1
+        parsed_fields.update({field: dbfield.verbose_name})
 
-def generate_excell(fields, model_instance):
+    instances = model_instance.objects.all()
+    return generate_excell(parsed_fields, instances)
+
+def generate_excell_load_fields(fields, model_instance):
+    for field in fields:
+        if fields[field]=='':
+            parts = field.split('.')
+            count = 1
+            dbfield = model_instance._meta.get_field(parts[0])
+            while count < len(parts):
+                dbfield = dbfield.related_model._meta.get_field(parts[count])
+                count += 1
+            fields[field] = dbfield.verbose_name
+
+    return generate_excell(fields, instances)
+    
+def generate_excell(fields, instances):
     response = HttpResponse(content_type='application/vnd.ms-excel')
     response['Content-Disposition'] = 'attachment; filename=Report.xlsx'
     output = BytesIO()
@@ -17,16 +43,8 @@ def generate_excell(fields, model_instance):
 
     col = 0
     for field in fields:
-        parts = field.split('.')
-        count = 1
-        dbfield = model_instance._meta.get_field(parts[0])
-        while count < len(parts):
-            dbfield = dbfield.related_model._meta.get_field(parts[count])
-            count += 1
-        worksheet_s.write(0, col, dbfield.verbose_name)
+        worksheet_s.write(0, col, fields[field])
         col += 1
-
-    instances = model_instance.objects.all()
 
     row = 1
     for instance in instances:
@@ -39,10 +57,7 @@ def generate_excell(fields, model_instance):
                 fieldvalue = getattr(fieldvalue, parts[count])
                 count += 1
             if fieldvalue is not None:
-                if isinstance(fieldvalue, str):
-                    worksheet_s.write(row, col, fieldvalue)
-                else:
-                    worksheet_s.write(row, col, fieldvalue)
+                worksheet_s.write(row, col, fieldvalue)
             col += 1
         row += 1
 
