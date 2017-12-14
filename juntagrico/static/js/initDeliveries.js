12b554ce4b9102cfef41503a1a6a0930907ef1af
Lynn Aders <lynn.aders@gmx.ch>
2017-12-14 21:30:11 +0100

    glyphicon right/down, first is down others right

--- juntagrico/static/js/initDeliveries.js
+++ juntagrico/static/js/initDeliveries.js
@@ -2,11 +2,11 @@
 define([], function () {
 
 	$('.collapse').on('shown.bs.collapse', function () {
-	    $(this).prev().prev().find(".glyphicon").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
+	    $(this).parent().find(".glyphicon").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
 	});
 	
 	//The reverse of the above on hidden event:	
 	$('.collapse').on('hidden.bs.collapse', function () {
-	    $(this).prev().prev().find(".glyphicon").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
+	    $(this).parent().find(".glyphicon").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
 	});
 });
