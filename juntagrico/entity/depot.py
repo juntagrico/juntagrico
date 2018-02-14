2283fd144a59bd9df1082bc320aa1643c1a77ae0
David Simmen <david.simmen@bluewin.ch>
2018-02-14 17:12:54 +0100

    flake8 of the day

--- juntagrico/entity/depot.py
+++ juntagrico/entity/depot.py
@@ -1,28 +1,35 @@
 # encoding: utf-8
 
 from django.core import validators
-from django.core.exceptions import ValidationError
 from django.db import models
-from juntagrico.util.temporal import *
+from juntagrico.util.temporal import weekday_choices, weekdays
 
 from juntagrico.dao.subscriptionsizedao import SubscriptionSizeDao
-from juntagrico.dao.extrasubscriptiontypedao import ExtraSubscriptionTypeDao
-from juntagrico.dao.extrasubscriptioncategorydao import ExtraSubscriptionCategoryDao
+from juntagrico.dao.extrasubscriptiontypedao.ExtraSubscriptionTypeDao \
+    import extra_types_by_category_ordered
+from juntagrico.dao.extrasubscriptioncategorydao \
+    import ExtraSubscriptionCategoryDao
+
 
 class Depot(models.Model):
     '''
     Location where stuff is picked up.
     '''
-    code = models.CharField('Code', max_length=100, validators=[validators.validate_slug], unique=True)
+    code = models.CharField('Code', max_length=100,
+                            validators=[validators.validate_slug], unique=True)
     name = models.CharField('Depot Name', max_length=100, unique=True)
     contact = models.ForeignKey('Member', on_delete=models.PROTECT)
     weekday = models.PositiveIntegerField('Wochentag', choices=weekday_choices)
-    latitude = models.CharField('Latitude', max_length=100, default='', null=True, blank=True)
-    longitude = models.CharField('Longitude', max_length=100, default='', null=True, blank=True)
-    addr_street = models.CharField('Strasse', max_length=100, null=True, blank=True)
-    addr_zipcode = models.CharField('PLZ', max_length=10, null=True, blank=True)
-    addr_location = models.CharField('Ort', max_length=50, null=True, blank=True)
-
+    latitude = models.CharField('Latitude', max_length=100, default='',
+                                null=True, blank=True)
+    longitude = models.CharField('Longitude', max_length=100, default='',
+                                 null=True, blank=True)
+    addr_street = models.CharField('Strasse', max_length=100,
+                                   null=True, blank=True)
+    addr_zipcode = models.CharField('PLZ', max_length=10,
+                                    null=True, blank=True)
+    addr_location = models.CharField('Ort', max_length=50,
+                                     null=True, blank=True)
     description = models.TextField('Beschreibung', max_length=1000, default='')
 
     overview_cache = None
@@ -36,13 +43,13 @@ class Depot(models.Model):
 
     @property
     def has_geo(self):
-        lat = self.latitude is not None and self.latitude!=''
-        long = self.longitude is not None and self.longitude!=''
-        street = self.addr_street is not None and self.addr_street !=''
-        zip = self.addr_zipcode is not None and self.addr_zipcode !=''
-        loc = self.addr_location is not None and self.addr_location !=''
-        return lat and long and street and zip and loc 
-    
+        lat = self.latitude is not None and self.latitude != ''
+        long = self.longitude is not None and self.longitude != ''
+        street = self.addr_street is not None and self.addr_street != ''
+        zip = self.addr_zipcode is not None and self.addr_zipcode != ''
+        loc = self.addr_location is not None and self.addr_location != ''
+        return lat and long and street and zip and loc
+
     @property
     def weekday_name(self):
         day = 'Unbekannt'
@@ -61,18 +68,26 @@ class Depot(models.Model):
     def extra_subscription(subscriptions, code):
         amount = 0
         for subscription in subscriptions.all():
-            amount += len(subscription.extra_subscriptions.all().filter(type__name=code))
+            esubs = subscription.extra_subscriptions.all()
+            filtered_esubs = esubs.filter(type__name=code)
+            amount += len(filtered_esubs)
         return amount
 
     def fill_overview_cache(self):
         self.fill_active_subscription_cache()
         self.overview_cache = []
         for subscription_size in SubscriptionSizeDao.sizes_for_depot_list():
-            self.overview_cache.append(self.subscription_amounts(self.subscription_cache, subscription_size.name))
+            cache = self.subscription_cache
+            size_name = subscription_size.name
+            amounts = self.subscription_amounts(cache, size_name)
+            self.overview_cache.append(amounts)
         for category in ExtraSubscriptionCategoryDao.all_categories_ordered():
-            for extra_subscription in ExtraSubscriptionTypeDao.extra_types_by_category_ordered(category):
+            types = extra_types_by_category_ordered(category)
+            for extra_subscription in types:
                 code = extra_subscription.name
-                self.overview_cache.append(self.extra_subscription(self.subscription_cache, code))
+                cache = self.subscription_cache
+                esub = self.extra_subscription(cache, code)
+                self.overview_cache.append(esub)
 
     def fill_active_subscription_cache(self):
         self.subscription_cache = self.active_subscriptions()
