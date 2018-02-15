67b9e0ab49a61b6e3a0f0f4598766f292bef8b35
motu55 <david.simmen@bluewin.ch>
2018-02-15 23:21:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='juntagrico',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='GNU Lesser General Public License v3 (LGPLv3)',
