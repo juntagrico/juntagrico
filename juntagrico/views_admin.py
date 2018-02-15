3914371a66c933d55d5a213b69a51faeb7c39ef6
David Simmen <david.simmen@bluewin.ch>
2018-02-15 22:56:55 +0100

    climate test

--- juntagrico/views_admin.py
+++ juntagrico/views_admin.py
@@ -10,6 +10,10 @@ from juntagrico.dao.extrasubscriptiontypedao import ExtraSubscriptionTypeDao
 from juntagrico.dao.extrasubscriptiondao import ExtraSubscriptionDao
 from juntagrico.dao.mailtemplatedao import MailTemplateDao
 from juntagrico.dao.memberdao import MemberDao
+from juntagrico.dao.memberdao.MemberDao import \
+    (members_for_email_with_subscription,
+     members_for_email,
+     members_with_shares)
 from juntagrico.dao.subscriptiondao import SubscriptionDao
 from juntagrico.dao.subscriptionsizedao import SubscriptionSizeDao
 from juntagrico.models import *
@@ -43,20 +47,15 @@ def send_email_intern(request):
     emails = set()
     sender = request.POST.get('sender')
     if request.POST.get('allsubscription') == 'on':
-        members = MemberDao.members_for_email_with_subscription()
-        m_emails = members.values_list('email', flat=True)
+        members = members_for_email_with_subscription().values_list('email',
+                                                                    flat=True)
         emails.extend(m_emails)
     if request.POST.get('allshares') == 'on':
-        for member in MemberDao.members_for_email():
-            if member.share_set.count() > 0:
-                emails.add(member.email)
+        emails.extend(members_with_shares().values_list('email', flat=True))
     if request.POST.get('all') == 'on':
-        m_emails = MemberDao.members_for_email().values_list('email',
-                                                             flat=True)
-        emails.extend(m_email)
+        emails.extend(members_for_email().values_list('email', flat=True))
     if request.POST.get('recipients'):
-        recipients = re.split(r'\s*,?\s*', request.POST.get('recipients'))
-        emails.extend(recipients)
+        emails.extend(re.split(r'\s*,?\s*', request.POST.get('recipients')))
     if request.POST.get('allsingleemail'):
         emails |= set(request.POST.get('singleemail').split(' '))
 
@@ -64,7 +63,9 @@ def send_email_intern(request):
     append_attachements(request, attachements)
 
     if len(emails) > 0:
-        send_filtered_mail(request.POST.get('subject'), request.POST.get('message'), request.POST.get('textMessage'),
+        send_filtered_mail(request.POST.get('subject'),
+                           request.POST.get('message'),
+                           request.POST.get('textMessage'),
                            emails, attachements, sender=sender)
         sent = len(emails)
     return redirect('/my/mails/send/result/' + str(sent) + '/')
