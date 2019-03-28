9471ae5208e020ae07d9b45fde1bce276389a431
David Simmen <david.simmen@bluewin.ch>
2019-03-28 19:32:04 +0100

    do not disply shares if not enabled in the admin for sub types

--- juntagrico/admins/subscription_type_admin.py
+++ juntagrico/admins/subscription_type_admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
+from juntagrico.config import Config
+from juntagrico.util.addons import get_subtype_inlines
+
+
 class SubscriptionTypeAdmin(admin.ModelAdmin):
     inlines = []
 
     def __init__(self, *args, **kwargs):
-        from juntagrico.util.addons import get_subtype_inlines
         self.inlines.extend(get_subtype_inlines())
         super(SubscriptionTypeAdmin, self).__init__(*args, **kwargs)
+
+    def get_exclude(self, request, obj=None):
+        if not Config.enable_shares():
+            return ['shares'] if self.exclude is None else self.exclude.append('shares')
+        return self.exclude
