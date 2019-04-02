fd2b9c76122c26cc2eef292e8dc27d29328a9c28
David Simmen <david.simmen@bluewin.ch>
2019-04-02 13:28:47 +0200

    fixing tests

--- manage.py
+++ manage.py
@@ -2,9 +2,8 @@
 import os
 import sys
 
+from django.core.management import execute_from_command_line
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsettings")
-
-    from django.core.management import execute_from_command_line
-
-execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
