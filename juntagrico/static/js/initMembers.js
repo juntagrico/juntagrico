0ff03719708d2f0b10859feb62327e3939e557bd
David Simmen <david.simmen@bluewin.ch>
2019-04-23 08:59:21 +0200

    js cleanup and duplicate code removal

--- juntagrico/static/js/initMembers.js
+++ juntagrico/static/js/initMembers.js
@@ -5,70 +5,16 @@ define([], function () {
         $(this).append("<input type='text' placeholder='' style='width: 100%;' class='form-control input-sm' />");
     });
 
-    var table = $("#filter-table").DataTable({
-        "paging": false,
-        "info": false,
-        "search": {
-            "regex": true,
-            "smart": false
-        },
-        "drawCallback": function (settings) {
-            // do not like this but it works so far till i get around to find the correct api call
-            updateSendEmailButton($("#filter-table tr").length - 2);
-        },
-        "language": {
-            "search": "Suchen: "
-        }
-    });
-    
-    decorate_man_list_inputs()
+    var table = default_data_table();
 
-    $("#filter-table_filter").each(function () {
-        $(this).css("text-align","right");
-    });
+    decorate_man_list_inputs();
 
-    function updateSendEmailButton(count) {
-        if (count == 0) {
-            $("button#send-email")
-                .prop("disabled", true)
-                .text(email_string);
-        } else if (count == 1) {
-            $("button#send-email")
-                .prop("disabled", false)
-                .text(email_single_string+" "+member_string+" "+send_string);
-        } else {
-            $("button#send-email")
-                .prop("disabled", false)
-                .text( email_multi_string+" " + count + " "+members_string+" "+send_string);
-        }
-    }
+    align_filter();
 
-    table.columns().every(function () {
-        var that = this;
-        $("input", this.header()).on("keyup change", function () {
-            if (that.search() !== this.value) {
-                that.search(this.value).draw();
-            }
-        });
-        $("input", this.header()).on("click", function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-        });
-    });
+    table_column_search(table);
 
-    // Move the "Send email" button (and the corresponding form) to the same level as the filter input
-    $("form#email-sender").appendTo("#filter_header div:first-child");
+    move_email_button();
 
-    $("form#email-sender").submit(function (event) {
-        var emails = [];
-        $("#filter-table").find("tr").each(function () {
-            var txt = $(".email", this).text().trim();
-            if (txt.length > 0)
-                emails.push(txt);
-        });
-        $("#recipients").val(emails.join("\n"));
-        $("#recipients_count").val(emails.length);
-        return;
-    });
+    email_submit();
 
 });
--- juntagrico/static/js/initSubscriptions.js
+++ juntagrico/static/js/initSubscriptions.js
@@ -4,60 +4,15 @@ define([], function () {
         $(this).append("<input type='text' placeholder='' style='width: 100%;' class='form-control input-sm' />");
     });
 
-    var table = $("#filter-table").DataTable({
-        "paging": false,
-        "info": false,
-        "search": {
-            "regex": true,
-            "smart": false
-        },
-        "drawCallback": function (settings) {
-            // do not like this but it works so far till i get around to find the correct api call
-            updateSendEmailButton($("#filter-table tr").length - 2);
-        },
-        "language": {
-            "search": "Suchen: "
-        }
-    });
+    var table = default_data_table();
 
-    decorate_man_list_inputs()
+    decorate_man_list_inputs();
 
-    $("#filter-table_filter").each(function () {
-        $(this).css("text-align","right");
-    });
+    align_filter();
 
-    function updateSendEmailButton(count) {
-        if (count == 0) {
-            $("button#send-email")
-                .prop("disabled", true)
-                .text(email_string);
-        } else if (count == 1) {
-            $("button#send-email")
-                .prop("disabled", false)
-                .text(email_single_string+" "+member_string+" "+send_string);
-        } else {
-            $("button#send-email")
-                .prop("disabled", false)
-                .text( email_multi_string+" " + count + " "+members_string+" "+send_string);
-        }
-    }
+    table_column_search(table);
 
-    table.columns().every(function () {
-        var that = this;
-        $("input", this.header()).on("keyup change", function () {
-            if (that.search() !== this.value) {
-                that.search(this.value,true,false).draw();
-            }
-        });
-        $("input", this.header()).on("click", function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-        });
-    });
-
-    // Move the "Send email" button (and the corresponding form) to the same level as the filter input
-    $("form#email-sender").appendTo("#filter-table_header div:first-child");
-
+    move_email_button();
 
     $("form#email-sender").submit(function (event) {
         var emails = [];
