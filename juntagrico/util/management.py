48f47f2c615b2883e48e737ba780e09d5d0fe232
David Simmen <david.simmen@bluewin.ch>
2019-04-23 08:00:03 +0200

    remove duplicate code

--- juntagrico/util/management.py
+++ juntagrico/util/management.py
@@ -19,43 +19,30 @@ def create_subscription(start_date, depot, selectedsubscription):
     subscription.start_date = start_date
     subscription.depot = depot
     subscription.save()
-    type = SubscriptionTypeDao.get_by_id(int(selectedsubscription))[0]
-    TSST.objects.create(type=type, subscription=subscription)
-    TFSST.objects.create(type=type, subscription=subscription)
+    sub_type = SubscriptionTypeDao.get_by_id(int(selectedsubscription))[0]
+    TSST.objects.create(type=sub_type, subscription=subscription)
+    TFSST.objects.create(type=sub_type, subscription=subscription)
     return subscription
 
 
 def create_member(member, subscription, main_member=None, shares=None):
-    if subscription is not None:
-        if subscription.state == 'waiting':
-            member.future_subscription = subscription
-        elif subscription.state == 'inactive':
-            member.old_subscriptions.add(subscription)
-        else:
-            member.subscription = subscription
-    member.future_subscription = subscription
+    add_subscription_to_member(member, subscription)
     member.save()
     password = password_generator()
     member.user.set_password(password)
     member.user.save()
     key = (member.email + str(member.id)).encode('utf8')
-    hash = hashlib.sha1(key).hexdigest()
+    member_hash = hashlib.sha1(key).hexdigest()
     if main_member is None:
-        send_welcome_mail(member.email, password, hash, subscription)
+        send_welcome_mail(member.email, password, member_hash, subscription)
     else:
         name = main_member.get_name()
         email = member.email
-        send_been_added_to_subscription(email, password, hash, name, shares)
+        send_been_added_to_subscription(email, password, member_hash, name, shares)
 
 
 def update_member(member, subscription, main_member=None, shares=None):
-    if subscription is not None:
-        if subscription.state == 'waiting':
-            member.future_subscription = subscription
-        elif subscription.state == 'inactive':
-            member.old_subscriptions.add(subscription)
-        else:
-            member.subscription = subscription
+    add_subscription_to_member(member, subscription)
     member.save()
     if main_member is not None:
         name = main_member.get_name()
@@ -66,3 +53,13 @@ def update_member(member, subscription, main_member=None, shares=None):
 def create_share(member):
     share = Share.objects.create(member=member)
     send_share_created_mail(member, share)
+
+
+def add_subscription_to_member(member, subscription):
+    if subscription is not None:
+        if subscription.state == 'waiting':
+            member.future_subscription = subscription
+        elif subscription.state == 'inactive':
+            member.old_subscriptions.add(subscription)
+        else:
+            member.subscription = subscription
