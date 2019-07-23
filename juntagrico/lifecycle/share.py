6ae164136b9b4611810d0efead2f3503c4881f6a
David Simmen <david.simmen@bluewin.ch>
2019-07-23 19:49:04 +0200

    fixes in share check

--- juntagrico/lifecycle/share.py
+++ juntagrico/lifecycle/share.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 from django.utils.translation import gettext as _
 
 from juntagrico.config import Config
@@ -21,11 +22,15 @@ def check_share_consistency(instance):
     is_canceled = instance.cancelled_date is not None
     is_terminated = instance.termination_date is not None
     is_payed_back = instance.payback_date is not None
+    paid_date = instance.paid_date or timezone.now()
+    cancelled_date = instance.cancelled_date or paid_date
+    termination_date = instance.termination_date or cancelled_date
+    payback_date = instance.payback_date  or termination_date
     if (is_canceled or is_terminated or is_payed_back) and not is_paid:
         raise ValidationError(_('Bitte "Bezahlt am" ausfüllen'), code='invalid')
     if (is_terminated or is_payed_back) and not is_canceled:
         raise ValidationError(_('Bitte "Gekündigt am" ausfüllen'), code='invalid')
     if is_payed_back and not is_terminated:
         raise ValidationError(_('Bitte "Gekündigt auf" ausfüllen'), code='invalid')
-    if not(instance.paid_date <= instance.cancelled_date <= instance.termination_date <= instance.payback_date):
+    if not(paid_date <= cancelled_date <= termination_date <= payback_date):
         raise ValidationError(_('Daten Reihenfolge stimm nicht.'), code='invalid')
