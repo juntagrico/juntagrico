5b443c6303fc0fb958fad5d98cb6befeba4f9b8c
David Simmen <david.simmen@bluewin.ch>
2019-09-04 21:07:51 +0200

    pep8

--- juntagrico/urls.py
+++ juntagrico/urls.py
@@ -35,21 +35,31 @@ urlpatterns = [
 
     # subscription related juntagrico stuff
     path('my/subscription/detail/', juntagrico_subscription.subscription, name='sub-detail'),  #
-    path('my/subscription/detail/<int:subscription_id>/', juntagrico_subscription.subscription, name='sub-detail-id'),  #
-    path('my/subscription/change/overview/<int:subscription_id>/', juntagrico_subscription.subscription_change, name='sub-change'),  #
-    path('my/subscription/change/depot/<int:subscription_id>/', juntagrico_subscription.depot_change, name='depot-change'),  #
-    path('my/subscription/change/size/<int:subscription_id>/', juntagrico_subscription.size_change, name='size-change'),  #
-    path('my/subscription/change/extra/<int:subscription_id>/', juntagrico_subscription.extra_change, name='extra-change'),
+    path('my/subscription/detail/<int:subscription_id>/', juntagrico_subscription.subscription, name='sub-detail-id'),
+    #
+    path('my/subscription/change/overview/<int:subscription_id>/', juntagrico_subscription.subscription_change,
+         name='sub-change'),  #
+    path('my/subscription/change/depot/<int:subscription_id>/', juntagrico_subscription.depot_change,
+         name='depot-change'),  #
+    path('my/subscription/change/size/<int:subscription_id>/', juntagrico_subscription.size_change, name='size-change'),
+    #
+    path('my/subscription/change/extra/<int:subscription_id>/', juntagrico_subscription.extra_change,
+         name='extra-change'),
     path('my/signup/', juntagrico_subscription.SignupView.as_view(), name='signup'),
     path('my/cosubmember/<int:subscription_id>/', juntagrico_subscription.AddCoMemberView.as_view(), name='add-member'),
     path('my/confirm/<str:hash>/', juntagrico_subscription.confirm, name='confirm'),
-    path('my/subscription/activate/<int:subscription_id>/', juntagrico_subscription.activate_subscription, name='sub-activate'),  #
-    path('my/subscription/deactivate/<int:subscription_id>/', juntagrico_subscription.deactivate_subscription, name='sub-deactivate'),  #
-    path('my/subscription/cancel/<int:subscription_id>/', juntagrico_subscription.cancel_subscription, name='sub-cancel'),
-    path('my/type/change/<int:subscription_id>/', juntagrico_subscription.activate_future_types, name='activate-future-types'),
+    path('my/subscription/activate/<int:subscription_id>/', juntagrico_subscription.activate_subscription,
+         name='sub-activate'),  #
+    path('my/subscription/deactivate/<int:subscription_id>/', juntagrico_subscription.deactivate_subscription,
+         name='sub-deactivate'),  #
+    path('my/subscription/cancel/<int:subscription_id>/', juntagrico_subscription.cancel_subscription,
+         name='sub-cancel'),
+    path('my/type/change/<int:subscription_id>/', juntagrico_subscription.activate_future_types,
+         name='activate-future-types'),
     path('my/extra/activate/<int:extra_id>/', juntagrico_subscription.activate_extra, name='extra-activate'),
     path('my/extra/deactivate/<int:extra_id>/', juntagrico_subscription.deactivate_extra, name='extra-deactivate'),
-    path('my/extra/cancel/<int:extra_id>/<int:subscription_id>/', juntagrico_subscription.cancel_extra, name='extra-cancel'),
+    path('my/extra/cancel/<int:extra_id>/<int:subscription_id>/', juntagrico_subscription.cancel_extra,
+         name='extra-cancel'),
     path('my/order/share/', juntagrico_subscription.order_shares, name='share-order'),
     path('my/order/share/success', juntagrico_subscription.order_shares_success, name='share-order-success'),
     path('my/payout/share/<int:share_id>/', juntagrico_subscription.payout_share, name='share-payout'),
@@ -76,16 +86,17 @@ urlpatterns = [
     path('my/filters/depot/<int:depot_id>/', juntagrico_admin.filters_depot, name='filter-depot'),
     path('my/filters/area/<int:area_id>/', juntagrico_admin.filters_area, name='filter-area'),
     path('my/subscriptions', juntagrico_admin.subscriptions, name='filter-subs'),  #
-    path('my/subscriptions/depot/<int:depot_id>/', juntagrico_admin.filter_subscriptions_depot, name='filter-subs-depot'),  #
+    path('my/subscriptions/depot/<int:depot_id>/', juntagrico_admin.filter_subscriptions_depot,
+         name='filter-subs-depot'),  #
     path('my/future', juntagrico_admin.future, name='future'),
     path('my/export', juntagrico_admin.export, name='export'),  #
     path('my/export/membersfilter', juntagrico_admin.excel_export_members_filter, name='export-membersfilter'),  #
-    path('my/export/members', juntagrico_admin.excel_export_members, name='export-members'), #
-    path('my/export/shares', juntagrico_admin.excel_export_shares, name='export-shares'), #
+    path('my/export/members', juntagrico_admin.excel_export_members, name='export-members'),  #
+    path('my/export/shares', juntagrico_admin.excel_export_shares, name='export-shares'),  #
     path('my/mailtemplate/<int:template_id>/', juntagrico_admin.get_mail_template, name='mail-template'),
     path('my/waitinglist', juntagrico_admin.waitinglist, name='sub-mgmt-waitinglist'),  #
     path('my/canceledlist', juntagrico_admin.canceledlist, name='sub-mgmt-canceledlist'),  #
-    path('my/typechangedlist', juntagrico_admin.typechangelist, name='sub-mgmt-changelist'), #
+    path('my/typechangedlist', juntagrico_admin.typechangelist, name='sub-mgmt-changelist'),  #
     path('my/extra/waitinglist', juntagrico_admin.extra_waitinglist, name='sub-mgmt-extra-waitinglist'),  #
     path('my/extra/canceledlist', juntagrico_admin.extra_canceledlist, name='sub-mgmt-extra-canceledlist'),  #
     path('my/share/canceledlist', juntagrico_admin.share_canceledlist, name='share-mgmt-canceledlist'),  #
