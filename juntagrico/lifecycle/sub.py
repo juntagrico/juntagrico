6f7f8a7b6fc77a9a96f202cfb4f2fa6d42309e1d
David Glenck <pascal.free@ihdg.ch>
2019-11-24 22:54:29 +0100

    gender

--- juntagrico/lifecycle/sub.py
+++ juntagrico/lifecycle/sub.py
@@ -51,5 +51,5 @@ def check_sub_consistency(instance):
     pm_active = instance.primary_member in instance.recipients_all_for_state('active')
     if instance.primary_member is not None and not (pm_waiting or pm_active):
         raise ValidationError(
-            _('Hauptbezieher muss auch {}-Bezieher sein').format(Config.vocabulary('subscription')),
+            _('HauptbezieherIn muss auch {}-BezieherIn sein').format(Config.vocabulary('subscription')),
             code='invalid')
