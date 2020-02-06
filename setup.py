b2a1b91eebb1e85ec6e04da66d62be3c1fda3787
David Simmen <david.simmen@bluewin.ch>
2020-02-06 22:00:19 +0100

    dev version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='juntagrico',
-    version='1.2.2',
+    version='1.2.2dev',
     packages=find_packages(),
     include_package_data=True,
     license='GNU Lesser General Public License v3 (LGPLv3)',
