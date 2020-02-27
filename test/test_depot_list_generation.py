77bb8f02a9e7915c9e03b306a16e136d3c11508b
David Simmen <david.simmen@bluewin.ch>
2020-02-27 13:15:49 +0100

    remove timing from test

--- test/test_depot_list_generation.py
+++ test/test_depot_list_generation.py
@@ -10,7 +10,5 @@ class DepotlistTests(JuntagricoTestCase):
 
     def test_depot_list(self):
         out = StringIO()
-        print(timezone.now().timestamp())
         call_command('generate_depot_list', '--force', stdout=out)
         self.assertEqual(out.getvalue(), '')
-        print(timezone.now().timestamp())
