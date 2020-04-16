5e9fbd03ba6751aaf31af1ec90bb15f5680c7868
David Simmen <david.simmen@bluewin.ch>
2020-04-16 09:02:48 +0200

    climate

--- juntagrico/lifecycle/extrasub.py
+++ juntagrico/lifecycle/extrasub.py
@@ -7,10 +7,3 @@ from juntagrico.util.lifecycle import handle_activated_deactivated
 def extra_sub_pre_save(sender, instance, **kwargs):
     handle_activated_deactivated(instance, sender, extra_sub_activated, extra_sub_deactivated)
 
-
-def handle_extra_sub_activated(sender, instance, **kwargs):
-    instance.activation_date = instance.activation_date or timezone.now().date()
-
-
-def handle_extra_sub_deactivated(sender, instance, **kwargs):
-    instance.deactivation_date = instance.deactivation_date or timezone.now().date()
--- juntagrico/lifecycle/subpart.py
+++ juntagrico/lifecycle/subpart.py
@@ -1,16 +1,6 @@
-from django.utils import timezone
-
 from juntagrico.signals import sub_part_activated, sub_part_deactivated
 from juntagrico.util.lifecycle import handle_activated_deactivated
 
 
 def sub_part_pre_save(sender, instance, **kwargs):
     handle_activated_deactivated(instance, sender, sub_part_activated, sub_part_deactivated)
-
-
-def handle_sub_part_activated(sender, instance, **kwargs):
-    instance.activation_date = instance.activation_date or timezone.now().date()
-
-
-def handle_sub_part_deactivated(sender, instance, **kwargs):
-    instance.deactivation_date = instance.deactivation_date or timezone.now().date()
