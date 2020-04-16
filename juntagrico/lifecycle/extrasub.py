d6955e816aafa9e6280f046597043f1e3442b7fe
David Simmen <david.simmen@bluewin.ch>
2020-04-16 09:04:10 +0200

    climate

--- juntagrico/lifecycle/extrasub.py
+++ juntagrico/lifecycle/extrasub.py
@@ -1,9 +1,6 @@
-from django.utils import timezone
-
 from juntagrico.signals import extra_sub_activated, extra_sub_deactivated
 from juntagrico.util.lifecycle import handle_activated_deactivated
 
 
 def extra_sub_pre_save(sender, instance, **kwargs):
     handle_activated_deactivated(instance, sender, extra_sub_activated, extra_sub_deactivated)
-
