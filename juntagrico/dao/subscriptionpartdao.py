f65eda96212617a7ab72517d13d0f85492f4c898
David Glenck <pascal.free@ihdg.ch>
2020-04-22 23:23:06 +0200

    fix

--- juntagrico/dao/subscriptionpartdao.py
+++ juntagrico/dao/subscriptionpartdao.py
@@ -1,12 +1,13 @@
 from juntagrico.entity.subs import SubscriptionPart
+from juntagrico.util.models import q_activated, q_cancelled
 
 
 class SubscriptionPartDao:
 
     @staticmethod
     def get_canceled_for_subscription(subscription):
-        return SubscriptionPart.objects.filter(subscription=subscription, cancellation_date__isnull=False)
+        return SubscriptionPart.objects.filter(subscription=subscription).filter(q_cancelled)
 
     @staticmethod
     def get_waiting_for_subscription(subscription):
-        return SubscriptionPart.objects.filter(subscription=subscription, activation_date__isnull=False)
+        return SubscriptionPart.objects.filter(subscription=subscription).filter(~q_activated)
