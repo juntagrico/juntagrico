b99977acfe67606e00dd8bb9ed426e03d7fe7771
David Simmen <david.simmen@bluewin.ch>
2020-05-01 08:24:22 +0200

    use date value as well to detect activation etc

--- juntagrico/util/models.py
+++ juntagrico/util/models.py
@@ -3,6 +3,7 @@
     It will only copy the fields with are present in both
 '''
 from django.db.models import Q
+from django.utils import timezone
 
 
 def attribute_copy(source, target):
@@ -14,10 +15,10 @@ def attribute_copy(source, target):
             target.__dict__[field.attname] = source.__dict__[field.attname]
 
 
-q_activated = Q(activation_date__isnull=False)
+q_activated = Q(activation_date__isnull=False, activation_date__gte=timezone.now().date())
 
 
-q_cancelled = Q(cancellation_date__isnull=False)
+q_cancelled = Q(cancellation_date__isnull=False, cancellation_date__gte=timezone.now().date())
 
 
-q_deactivated = Q(deactivation_date__isnull=False)
+q_deactivated = Q(deactivation_date__isnull=False, deactivation_date__gte=timezone.now().date())
