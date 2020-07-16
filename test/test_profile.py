87a5a81cff21afbbf948cbdaa00cea3b80faca57
David Simmen <david.simmen@bluewin.ch>
2020-07-16 10:30:26 +0200

    views test coverage 100%

--- test/test_profile.py
+++ test/test_profile.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.urls import reverse
 from django.utils import timezone
 
@@ -10,13 +11,23 @@ class ProfileTests(JuntagricoTestCase):
         self.assertGet(reverse('profile'))
 
     def testProfilePost(self):
-        self.assertPost(reverse('profile'), {'iban': 'iban'})
+        self.assertPost(reverse('profile'), {'iban': 'CH29 0900 0000 9000 1480 3',
+                                             'email': 'test@juntagrico.org',
+                                             'addr_street': 'addr_street',
+                                             'addr_zipcode': ' 1234',
+                                             'addr_location': 'addr_location',
+                                             'phone': 'phone'})
 
     def testCancelMembership(self):
         self.assertGet(reverse('cancel-membership'))
 
     def testCancelMembershipPost(self):
         self.assertPost(reverse('cancel-membership'), code=302)
+
+    def testCancelMembershipNonCoopPost(self):
+        self.assertPost(reverse('cancel-membership'), code=302, member=self.member3)
+        self.member3.refresh_from_db()
+        self.assertTrue(self.member3.inactive)
 
     def testDeactivateMembership(self):
         # must first cancel and pay back the shares
@@ -30,3 +41,24 @@ class ProfileTests(JuntagricoTestCase):
         self.assertPost(reverse('member-deactivate', args=(self.member.pk,)), code=302)
         self.member.refresh_from_db()
         self.assertTrue(self.member.inactive)
+
+    def testConfirmEmail(self):
+        self.assertGet(reverse('send-confirm'))
+        self.assertEqual(len(mail.outbox), 1)
+
+    def testChangePassword(self):
+        self.assertGet(reverse('password'))
+
+    def testChangePasswordPost(self):
+        self.assertPost(reverse('password'), {'password': 'password',
+                                              'passwordRepeat': 'password'})
+    def testNewPassword(self):
+        self.assertGet(reverse('new-password'))
+        self.assertEqual(len(mail.outbox), 0)
+
+    def testNewPasswordPost(self):
+        self.assertPost(reverse('new-password'), {'username': 'email3@email.org'})
+        self.assertEqual(len(mail.outbox), 1)
+
+    def testLogout(self):
+        self.assertGet(reverse('logout'), code=302)
