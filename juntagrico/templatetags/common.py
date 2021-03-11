29431bc01b5e0c7501ea89adb9031bb4c34b6e08
David Simmen <david.simmen@bluewin.ch>
2021-03-11 18:35:16 +0100

    improved version handling

--- juntagrico/templatetags/common.py
+++ juntagrico/templatetags/common.py
@@ -1,5 +1,7 @@
 from django import template
 
+from juntagrico import version
+
 register = template.Library()
 
 
@@ -8,3 +10,8 @@ def get_item(dictionary, key):
     if not hasattr(dictionary, 'get'):
         return False
     return dictionary.get(key)
+
+
+@register.simple_tag
+def get_version():
+    return version
