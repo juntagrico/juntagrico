d97bd17f0bdbcfd2375e2ae0146d9329d1c5e00e
David Glenck <pascal.free@ihdg.ch>
2021-06-10 23:09:03 +0200

    put job assigned message first

--- juntagrico/util/messages.py
+++ juntagrico/util/messages.py
@@ -23,26 +23,22 @@ def home_messages(request):
 
 
 def job_messages(request, job):
-    result = []
     member = request.user.member
     all_participants = list(MemberDao.members_by_job(job))
-    if job.canceled:
-        result.append(get_template('messages/job_canceled.html').render())
-    elif job.end_time() < timezone.now():
-        result.append(get_template('messages/job_past.html').render())
-    elif job.start_time() < timezone.now():
-        result.append(get_template('messages/job_running.html').render())
-    elif member in all_participants:
+    if member in all_participants:
         render_dict = {
             'amount': all_participants.count(member) - 1,
         }
-        template = get_template('messages/job_assigned.html')
-        render_result = template.render(render_dict)
-        result.append(render_result)
-
+        return [get_template('messages/job_assigned.html').render(render_dict)]
+    elif job.canceled:
+        return [get_template('messages/job_canceled.html').render()]
+    elif job.end_time() < timezone.now():
+        return [get_template('messages/job_past.html').render()]
+    elif job.start_time() < timezone.now():
+        return [get_template('messages/job_running.html').render()]
     elif job.free_slots == 0:
-        result.append(get_template('messages/job_fully_booked.html').render())
-    return result
+        return [get_template('messages/job_fully_booked.html').render()]
+    return []
 
 
 def error_message(request):
