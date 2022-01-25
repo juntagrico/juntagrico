2d470cf6cf77bcb0b4f70ccc97502b838bc70bbb
David Glenck <pascal.free@ihdg.ch>
2022-01-25 03:01:01 +0100

    #421 hint on address and coordinates in admin

--- juntagrico/admins/location_admin.py
+++ juntagrico/admins/location_admin.py
@@ -11,6 +11,19 @@ from juntagrico.admins.forms.location_replace_form import LocationReplaceForm
 class LocationAdmin(SortableAdminMixin, RichTextAdmin):
     list_display = ['name', 'addr_street', 'addr_zipcode', 'addr_location', 'latitude', 'longitude', 'visible']
     actions = ['replace']
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'visible')
+        }),
+        (_('Adresse'), {
+            'description': _('Wenn Adresse nicht leer ist, wird ein Link zu einer Karte angezeigt.'),
+            'fields': ('addr_street', ('addr_zipcode', 'addr_location')),
+        }),
+        (_('Koordinaten'), {
+            'description': _('Wenn Koordinaten nicht leer sind, wird eine Karte eingeblendet.'),
+            'fields': (('latitude', 'longitude'),),
+        }),
+    )
 
     @admin.action(description=_('Ausgewählte Orte ersetzen'))
     def replace(self, request, queryset):
