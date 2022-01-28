f8971300419acde9b0c784d8e3bf8e5fdf912b39
David Glenck <github@ihdg.ch>
2022-01-28 11:45:26 +0100

    flake

--- juntagrico/juntagricoapp.py
+++ juntagrico/juntagricoapp.py
@@ -2,7 +2,7 @@ from juntagrico.util import addons
 
 # register template hooks
 
-addons.config.template.register('user_menu', ['menu/entries/'+n+'.html' for n in [
+addons.config.template.register('user_menu', ['menu/entries/' + n + '.html' for n in [
     'subscription',
     'jobs',
     'deliveries',
@@ -11,12 +11,12 @@ addons.config.template.register('user_menu', ['menu/entries/'+n+'.html' for n in
     'contact'
 ]])
 
-addons.config.template.register('user_menu_jobs', ['menu/entries/jobs/'+n+'.html' for n in [
+addons.config.template.register('user_menu_jobs', ['menu/entries/jobs/' + n + '.html' for n in [
     'my',
     'all'
 ]])
 
-addons.config.template.register('home', ['widgets/home/'+n+'.html' for n in [
+addons.config.template.register('home', ['widgets/home/' + n + '.html' for n in [
     'jobs',
     'activityareas'
 ]])
