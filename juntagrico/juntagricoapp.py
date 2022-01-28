d45cddf298aaa37cb0d1f0acf88f0d7895554e9c
David Glenck <pascal.free@ihdg.ch>
2022-01-28 04:46:44 +0100

    template hooks poc

---
+++ juntagrico/juntagricoapp.py
@@ -0,0 +1,24 @@
+from juntagrico.util import addons
+
+# register template hooks
+
+addons.config.template.register('user_menu', ['menu/entries/'+n+'.html' for n in [
+    'subscription',
+    'jobs',
+    'deliveries',
+    'activityareas',
+    'membership',
+    'contact'
+]])
+
+addons.config.template.register('user_menu_jobs', ['menu/entries/jobs/'+n+'.html' for n in [
+    'my',
+    'all'
+]])
+
+addons.config.template.register('home', ['widgets/home/'+n+'.html' for n in [
+    'jobs',
+    'activityareas'
+]])
+
+addons.config.template.register('pre_job')
