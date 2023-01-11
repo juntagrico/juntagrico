168dabfd72704642326d77e59f1449ebcb67aacc
pascalfree <pascal.free@ihdg.ch>
2023-01-11 01:07:20 +0100

    #507 make unique before migration

--- juntagrico/migrations/0039_data_1_6.py
+++ juntagrico/migrations/0039_data_1_6.py
@@ -3,6 +3,17 @@
 from django.db import migrations
 
 
+def make_name_product_unique(apps, schema_editor):
+    SubscriptionSize = apps.get_model('juntagrico', 'SubscriptionSize')
+    taken = set()
+    for size in SubscriptionSize.objects.all():
+        name_product = (size.name, size.product)
+        if name_product in taken:
+            size.name += str(size.id)
+            size.save()
+        taken.add(name_product)
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -10,4 +21,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(make_name_product_unique),
     ]
