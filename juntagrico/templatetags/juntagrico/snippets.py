e712839262b219d8ec5755789cb7c8f2340a292f
pascalfree <pascal.free@ihdg.ch>
2023-01-12 03:26:47 +0100

    #523 separate impersonation link

---
+++ juntagrico/templatetags/juntagrico/snippets.py
@@ -0,0 +1,12 @@
+from django import template
+from impersonate.helpers import check_allow_for_user
+
+register = template.Library()
+
+@register.inclusion_tag('snippets/impersonation_link.html')
+def impersonate_start(request, member):
+    user = member.user
+    return {
+        'can_impersonate': check_allow_for_user(request, user),
+        'user': user
+    }
