422056ddfb6bac2d53913e438013207e14b35497
pascalfree <pascal.free@ihdg.ch>
2023-02-14 16:50:32 +0100

    run test against postgres

--- testsettings.py
+++ testsettings.py
@@ -36,6 +36,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'testdb',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 ROOT_URLCONF = 'testurls'
 
 AUTHENTICATION_BACKENDS = (
