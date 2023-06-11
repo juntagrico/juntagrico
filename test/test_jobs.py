d143d0d616d3e527958b89ed0ae03a66784f663c
Tom Cinbis <tom.cinbis@netlight.com>
2023-06-11 10:24:30 +0200

    fix: passing subscribe flag in job tests

--- test/test_jobs.py
+++ test/test_jobs.py
@@ -19,35 +19,35 @@ class JobTests(JuntagricoTestCase):
         self.assertGet(reverse('memberjobs'))
 
     def testJobPost(self):
-        self.assertPost(reverse('job', args=[self.job1.pk]), {'jobs': 1}, 302)
+        self.assertPost(reverse('job', args=[self.job1.pk]), {'jobs': 1, 'subscribe': True}, 302)
         self.assertEqual(self.job1.free_slots, 0)
         self.assertEqual(self.job1.assignment_set.first().amount, 1)
 
     def testJobExtras(self):
-        self.assertPost(reverse('job', args=[self.job3.pk]), {'jobs': 1, 'extra' + str(self.job_extra_type.id): str(self.job_extra_type.id)}, 302)
+        self.assertPost(reverse('job', args=[self.job3.pk]), {'jobs': 1, 'extra' + str(self.job_extra_type.id): str(self.job_extra_type.id), 'subscribe': True}, 302)
         self.assertEqual(self.job3.assignment_set.first().job_extras.count(), 1)
         self.assertGet(reverse('job', args=[self.job3.pk]))
 
     def testMultipleEntries(self):
-        self.assertPost(reverse('job', args=[self.job4.pk]), {'jobs': 1}, 302)
+        self.assertPost(reverse('job', args=[self.job4.pk]), {'jobs': 1, 'subscribe': True}, 302)
         self.assertEqual(self.job4.assignment_set.count(), 1)
         self.assertGet(reverse('job', args=[self.job4.pk]))
-        self.assertPost(reverse('job', args=[self.job4.pk]), {'jobs': 1}, 302)
+        self.assertPost(reverse('job', args=[self.job4.pk]), {'jobs': 1, 'subscribe': True}, 302)
         self.assertEqual(self.job4.assignment_set.count(), 2)
         self.assertGet(reverse('job', args=[self.job4.pk]))
-        self.assertPost(reverse('job', args=[self.job4.pk]), {'jobs': 3}, 302)
+        self.assertPost(reverse('job', args=[self.job4.pk]), {'jobs': 3, 'subscribe': True}, 302)
         self.assertEqual(self.job4.assignment_set.count(), 5)
         self.assertGet(reverse('job', args=[self.job4.pk]))
 
     def testInfiniteSlots(self):
-        self.assertPost(reverse('job', args=[self.infinite_job.pk]), {'jobs': 3}, 302)
+        self.assertPost(reverse('job', args=[self.infinite_job.pk]), {'jobs': 3, 'subscribe': True}, 302)
         self.assertEqual(self.infinite_job.assignment_set.count(), 3)
         self.assertGet(reverse('job', args=[self.infinite_job.pk]))
 
     def testOverassignement(self):
-        self.assertPost(reverse('job', args=[self.job4.pk]), {'jobs': 1000})
+        self.assertPost(reverse('job', args=[self.job4.pk]), {'jobs': 1000, 'subscribe': True})
 
     def testHours(self):
         with self.settings(ASSIGNMENT_UNIT='HOURS'):
-            self.assertPost(reverse('job', args=[self.job5.pk]), {'jobs': 1}, 302)
+            self.assertPost(reverse('job', args=[self.job5.pk]), {'jobs': 1, 'subscribe': True}, 302)
             self.assertEqual(self.job5.assignment_set.first().amount, 2)
