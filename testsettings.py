1bdd8f8ad760fefc976ecd6dd3994dd4415ac77e
pascalfree <pascal.free@ihdg.ch>
2023-11-04 20:23:07 +0100

    upgrade fontawesome and install via python package

--- testsettings.py
+++ testsettings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'fontawesomefree',
     'impersonate',
     'juntagrico',
     'crispy_forms',
