96fe3f945b268dc51fb1d354a17e0d1736baa80a
pascalfree <pascal.free@ihdg.ch>
2024-01-07 04:21:59 +0100

    update build and publishing

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ import juntagrico
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
 
 def get_requirements(requirements_file):
     with open(requirements_file) as f:
@@ -19,31 +16,8 @@ def get_requirements(requirements_file):
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='juntagrico',
     version=juntagrico.version,
     packages=find_packages(),
     include_package_data=True,
-    license='GNU Lesser General Public License v3 (LGPLv3)',
-    description='juntagrico',
-    long_description=README,
-    url='https://www.juntagrico.org',
-    author='juntagrico',
-    author_email='python@juntagrico.org',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 4.2',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8 ',
-        'Programming Language :: Python :: 3.9 ',
-        'Programming Language :: Python :: 3.10 ',
-        'Programming Language :: Python :: 3.11 ',
-        'Topic :: Internet :: WWW/HTTP :: Site Management',
-    ],
     install_requires=get_requirements(os.path.join(ROOT_DIR, 'requirements.txt')),
 )
