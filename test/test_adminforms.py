4d1d25e4bc7bb4bf316c0b95f49331e7d012cf01
pascalfree <pascal.free@ihdg.ch>
2024-01-21 12:48:00 +0100

    fix mass job copy for USE_TZ=False
    (cherry picked from commit 314c836ad14b58de5ef04482a17aed6ecd794b08)

--- test/test_adminforms.py
+++ test/test_adminforms.py
@@ -1,9 +1,10 @@
 import datetime
 
 from django.core.exceptions import ValidationError
+from django.test import override_settings
 
 from juntagrico.admins.forms.delivery_copy_form import DeliveryCopyForm
-from juntagrico.admins.forms.job_copy_form import JobCopyForm
+from juntagrico.admins.forms.job_copy_form import JobCopyForm, JobCopyToFutureForm
 from juntagrico.admins.forms.location_replace_form import LocationReplaceForm
 from juntagrico.entity.delivery import Delivery
 from juntagrico.entity.jobs import RecuringJob
@@ -11,7 +12,7 @@ from juntagrico.entity.location import Location
 from test.util.test import JuntagricoTestCase
 
 
-class AdminTests(JuntagricoTestCase):
+class JobFormTests(JuntagricoTestCase):
 
     def testCopyJobForm(self):
         initial_count = RecuringJob.objects.all().count()
@@ -26,6 +27,7 @@ class AdminTests(JuntagricoTestCase):
         form = JobCopyForm(instance=self.job1, data=data)
         form.full_clean()
         with self.assertRaises(ValidationError):
+            # should raise, because no jobs are created in this date range
             form.clean()
         self.job1.time = datetime.datetime.now()
         data = {'type': self.job1.type.pk,
@@ -55,6 +57,47 @@ class AdminTests(JuntagricoTestCase):
         form.save()
         self.assertEqual(RecuringJob.objects.all().count(), initial_count + 2)
 
+    def testCopyJobToFutureForm(self):
+        # test failing case
+        initial_count = RecuringJob.objects.all().count()
+        data = {'type': self.job1.type.pk,
+                'time': '05:04:53',
+                'slots': '2',
+                'weekdays': ['1'],
+                'start_date': '01.07.2020',
+                'end_date': '07.07.2020',
+                'weekly': '7'
+                }
+        form = JobCopyToFutureForm(instance=self.job1, data=data)
+        form.full_clean()
+        with self.assertRaises(ValidationError) as validation_error:
+            # should raise, because jobs are in the past
+            form.clean()
+        self.assertEqual(validation_error.exception.code, 'date_in_past')
+
+        today = datetime.date.today()
+        # test successful case
+        data = {'type': self.job1.type.pk,
+                'time': '05:04:53',
+                'slots': '2',
+                'weekdays': ['1'],
+                'start_date': today + datetime.timedelta(1),
+                'end_date': today + datetime.timedelta(7),
+                'weekly': '7'
+                }
+        form = JobCopyToFutureForm(instance=self.job1, data=data)
+        form.full_clean()
+        form.clean()
+        form.save()
+        self.assertEqual(RecuringJob.objects.all().count(), initial_count + 1)
+
+
+@override_settings(USE_TZ=False)
+class JobFormWoTimezoneTests(JobFormTests):
+    pass
+
+
+class AdminTests(JuntagricoTestCase):
     def testDeliveryCopyForm(self):
         initial_count = Delivery.objects.all().count()
         data = {'subscription_size': self.sub_size.pk,
