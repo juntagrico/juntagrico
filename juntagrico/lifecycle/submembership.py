cf0004dd38448272c18018677087db7e8f2e9f69
pascalfree <pascal.free@ihdg.ch>
2024-07-11 01:07:47 +0200

    test validation response

--- juntagrico/lifecycle/submembership.py
+++ juntagrico/lifecycle/submembership.py
@@ -32,9 +32,11 @@ def check_submembership_parent_dates(instance):
     wrong_start = (m_joined and s_activated and subscription.activation_date > instance.join_date) or (not s_activated and m_joined)
     wrong_end = (m_left and s_deactivated and subscription.deactivation_date < instance.leave_date) or (s_deactivated and not m_left)
     if wrong_start:
-        raise ValidationError(_('Beitrittsdatum des Bestandteils passt nicht zum übergeordneten Aktivierungsdatum'), code='invalid')
+        raise ValidationError(_('Beitrittsdatum des Bestandteils passt nicht zum übergeordneten Aktivierungsdatum'),
+                              code='part_activation_date_mismatch')
     if wrong_end:
-        raise ValidationError(_('Austrittsdatum des Bestandteils passt nicht zum übergeordneten Deaktivierungsdatum'), code='invalid')
+        raise ValidationError(_('Austrittsdatum des Bestandteils passt nicht zum übergeordneten Deaktivierungsdatum'),
+                              code='part_deactivation_date_mismatch')
 
 
 def check_sub_membership_consistency(instance):
