20a7ff7640a50f76ecd7ae745f1ddb5feba44447
pascalfree <pascal.free@ihdg.ch>
2024-10-01 22:44:34 +0200

    update language files

--- make_language.sh
+++ make_language.sh
@@ -1,2 +1,2 @@
-python -m manage makemessages --all -i "venv*/**" -e html,txt,py,js
-python -m manage compilemessages -i "venv*/**"
+python -m manage makemessages --all -i ".venv*/**" -e html,txt,py,js
+python -m manage compilemessages -i ".venv*/**"
