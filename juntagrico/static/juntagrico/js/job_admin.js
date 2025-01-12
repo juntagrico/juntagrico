a5036977489abaecdac415bdab0fe4d9b551f6c5
pascalfree <pascal.free@ihdg.ch>
2025-01-12 15:57:44 +0100

    #752 fix: don't lead description and duration if no job type is selected

--- juntagrico/static/juntagrico/js/job_admin.js
+++ juntagrico/static/juntagrico/js/job_admin.js
@@ -2,7 +2,9 @@ $(function ($) {
     let description = $('#id_type_description')
     let duration = $('#id_type_duration')
     $('#id_type').on('change', function () {
-        description.load(description.data('url').replace('99', this.value))
-        duration.load(duration.data('url').replace('99', this.value))
+        if (this.value !== '') {
+            description.load(description.data('url').replace('99', this.value))
+            duration.load(duration.data('url').replace('99', this.value))
+        }
     })
 })
