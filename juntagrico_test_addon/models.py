fb62a27c2f0a857421f33a1565a102f70c298509
pascalfree <pascal.free@ihdg.ch>
2025-04-20 18:24:13 +0200

    climate

--- juntagrico_test_addon/models.py
+++ juntagrico_test_addon/models.py
@@ -7,4 +7,5 @@ from juntagrico.queryset.subscription import SubscriptionQuerySet
 def my_rounding(number):
     return Ceil(number*2)/2
 
+
 SubscriptionQuerySet._assignment_rounding = staticmethod(my_rounding)
