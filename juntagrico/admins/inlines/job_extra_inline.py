0cfefe699756a2e609ea63bda32acd325a3761f0
pascalfree <pascal.free@ihdg.ch>
2025-05-20 00:00:07 +0200

    #796 exclude inline fields explicitly

--- juntagrico/admins/inlines/job_extra_inline.py
+++ juntagrico/admins/inlines/job_extra_inline.py
@@ -5,7 +5,14 @@ from juntagrico.entity.jobs import JobExtra
 
 class JobExtraInline(admin.TabularInline):
     model = JobExtra
-    exclude = ['recuring_type', 'onetime_type']
 
     def get_extra(self, request, obj=None, **kwargs):
         return 0
+
+
+class JobExtraInlineForJobType(JobExtraInline):
+    exclude = ['onetime_type']
+
+
+class JobExtraInlineForOnetimeJob(JobExtraInline):
+    exclude = ['recuring_type']
