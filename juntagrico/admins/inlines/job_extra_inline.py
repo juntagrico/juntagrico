178cf19c7f4a2fa9b5817d789fafd5fc34ee1d32
pascalfree <pascal.free@ihdg.ch>
2025-05-19 23:44:53 +0200

    fix: hide fields in job extra inline

--- juntagrico/admins/inlines/job_extra_inline.py
+++ juntagrico/admins/inlines/job_extra_inline.py
@@ -5,6 +5,7 @@ from juntagrico.entity.jobs import JobExtra
 
 class JobExtraInline(admin.TabularInline):
     model = JobExtra
+    exclude = ['recuring_type', 'onetime_type']
 
     def get_extra(self, request, obj=None, **kwargs):
         return 0
