b4f8e3a710b94f269c63b79c898a0826d7065722
pascalfree <pascal.free@ihdg.ch>
2026-02-12 22:26:13 +0100

    #743 show categories in config overview

--- juntagrico/views/config.py
+++ juntagrico/views/config.py
@@ -1,11 +1,12 @@
 from django.db.models import F
 from django.shortcuts import render
 
-from juntagrico.entity.subtypes import SubscriptionProduct, SubscriptionBundle
+from juntagrico.entity.subtypes import SubscriptionProduct, SubscriptionBundle, SubscriptionCategory
 
 
 def overview(request):
     return render(request, 'juntagrico/config/overview.html', {
         'products': SubscriptionProduct.objects.all(),
+        'categories': SubscriptionCategory.objects.all(),
         'bundles': SubscriptionBundle.objects.order_by(F('category__sort_order').asc(nulls_last=True)),
     })
