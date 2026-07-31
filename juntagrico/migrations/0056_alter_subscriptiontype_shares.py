d8a160c8c3365d35a8b171d25ff26b68728a4581
pascalfree <pascal.free@ihdg.ch>
2026-07-31 10:55:42 +0200

    fix migrations

--- juntagrico/migrations/0056_alter_subscriptiontype_shares.py
+++ juntagrico/migrations/0056_alter_subscriptiontype_shares.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('juntagrico', '0054_member_cancellation_comment_and_more'),
+        ('juntagrico', '0055_jobmessage'),
     ]
 
     operations = [
